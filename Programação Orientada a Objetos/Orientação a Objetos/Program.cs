1d307ee489166623666d6cf1c7852ffeb377ae1b
guilhermelins7 <guilherme7lins@gmail.com>
2024-10-23 21:24:38 -0300

    Descomentado código orientação a objetos

--- Programação Orientada a Objetos/Orientação a Objetos/Program.cs
+++ Programação Orientada a Objetos/Orientação a Objetos/Program.cs
@@ -1,322 +1,322 @@
-﻿///*
-//Declare uma classe “Funcionario” que possui 2 (dois)
-//campos (nome do funcionário e salário). Em seguida
-//realize a instanciação de um objeto da respectiva classe.
-//*/
+﻿/*
+Declare uma classe “Funcionario” que possui 2 (dois)
+campos (nome do funcionário e salário). Em seguida
+realize a instanciação de um objeto da respectiva classe.
+*/
 
-//namespace Program
-//{
-//    public class Funcionario
-//    {
-//        public string Nome;
-//        public double Salario;
-//    }
-//    public class Program
-//    {
-//        static void Main()
-//        {
-//            Funcionario funcionarioUm = new Funcionario();
-//        }
-//    }
-//}
+namespace Program
+{
+    public class Funcionario
+    {
+        public string Nome;
+        public double Salario;
+    }
+    public class Program
+    {
+        static void Main()
+        {
+            Funcionario funcionarioUm = new Funcionario();
+        }
+    }
+}
 
-///*
-//Altere a classe do exercício anterior de tal a forma a
-//implementar um método construtor que realizar a
-//atribuição inicial para os campos da classe.
-//*/
+/*
+Altere a classe do exercício anterior de tal a forma a
+implementar um método construtor que realizar a
+atribuição inicial para os campos da classe.
+*/
 
-//namespace Program
-//{
-//    public class Funcionario
-//    {
-//        public string Nome;
-//        public double Salario;
-//        public Funcionario(string nome, double salario)
-//        {
-//            Nome = nome;
-//            Salario = salario;
-//        }
-//    }
-//    public class Program
-//    {
-//        static void Main()
-//        {
-//            string nome = "Guilherme";
-//            double salario = 100000.50;
-//            Funcionario funcionarioUm = new Funcionario(nome, salario);
-//        }
-//    }
-//}
+namespace Program
+{
+    public class Funcionario
+    {
+        public string Nome;
+        public double Salario;
+        public Funcionario(string nome, double salario)
+        {
+            Nome = nome;
+            Salario = salario;
+        }
+    }
+    public class Program
+    {
+        static void Main()
+        {
+            string nome = "Guilherme";
+            double salario = 100000.50;
+            Funcionario funcionarioUm = new Funcionario(nome, salario);
+        }
+    }
+}
 
-///*
-//Crie uma classe chamada “Nota Fiscal” que possa ser
-//utilizado por uma loja de suprimentos de tecnologia para
-//representar uma fatura de um item vendido na loja. Uma
-//Nota fiscal deve incluir as seguintes informações como
-//atributos: o número do item faturado; a descrição do item;
-//a quantidade comprada do item; o preço unitário do item.
-//Esta classe deve ter um construtor que inicialize os quatro
-//atributos. Além disso, forneça um método chamado
-//“getInvoiceAmount” que calcula o valor da fatura (isso é,
-//multiplica a quantidade pelo preço por item) e depois
-//retorna o valor como um double.
-//*/
+/*
+Crie uma classe chamada “Nota Fiscal” que possa ser
+utilizado por uma loja de suprimentos de tecnologia para
+representar uma fatura de um item vendido na loja. Uma
+Nota fiscal deve incluir as seguintes informações como
+atributos: o número do item faturado; a descrição do item;
+a quantidade comprada do item; o preço unitário do item.
+Esta classe deve ter um construtor que inicialize os quatro
+atributos. Além disso, forneça um método chamado
+“getInvoiceAmount” que calcula o valor da fatura (isso é,
+multiplica a quantidade pelo preço por item) e depois
+retorna o valor como um double.
+*/
 
-//namespace Program
-//{
-//    public class NotaFiscal
-//    {
-//        public int Numero;
-//        public string Descricao;
-//        public int Quantidade;
-//        public double Preco;
-//        public NotaFiscal(int numero, string descricao, int quantidade,
-//        double preco)
-//        {
-//            Numero = numero;
-//            Descricao = descricao;
-//            Quantidade = quantidade;
-//            Preco = preco;
-//        }
-//        public double GetInvoiceAmount()
-//        {
-//            return Quantidade * Preco;
-//        }
-//    }
-//    public class Program
-//    {
-//        static void Main()
-//        {
-//            int numero = 1;
-//            string descricao = "Teclado";
-//            int quantidade = 3;
-//            double preco = 720.50;
-//            NotaFiscal nota = new NotaFiscal(numero, descricao,
-//            quantidade, preco);
-//            double valorDaFatura = nota.GetInvoiceAmount();
-//            Console.WriteLine(valorDaFatura);
-//        }
-//    }
-//}
+namespace Program
+{
+    public class NotaFiscal
+    {
+        public int Numero;
+        public string Descricao;
+        public int Quantidade;
+        public double Preco;
+        public NotaFiscal(int numero, string descricao, int quantidade,
+        double preco)
+        {
+            Numero = numero;
+            Descricao = descricao;
+            Quantidade = quantidade;
+            Preco = preco;
+        }
+        public double GetInvoiceAmount()
+        {
+            return Quantidade * Preco;
+        }
+    }
+    public class Program
+    {
+        static void Main()
+        {
+            int numero = 1;
+            string descricao = "Teclado";
+            int quantidade = 3;
+            double preco = 720.50;
+            NotaFiscal nota = new NotaFiscal(numero, descricao,
+            quantidade, preco);
+            double valorDaFatura = nota.GetInvoiceAmount();
+            Console.WriteLine(valorDaFatura);
+        }
+    }
+}
 
-///*
-//Implemente uma classe “Pessoa” que possua como atributos nome,
-//idade, peso (em quilogramas) e altura (em metros). Faça com que 
-//os dados sejam inicializados através do construtor da classe. 
-//Adicione métodos para ler e alterar cada um dos atributos em separado.
-//*/
+/*
+Implemente uma classe “Pessoa” que possua como atributos nome,
+idade, peso (em quilogramas) e altura (em metros). Faça com que 
+os dados sejam inicializados através do construtor da classe. 
+Adicione métodos para ler e alterar cada um dos atributos em separado.
+*/
 
-//namespace Program
-//{
-//    public class Pessoa
-//    {
-//        private string _nome;
-//        private int _idade;
-//        private float _peso;
-//        private float _altura;
-//        public Pessoa(string nome, int idade, float peso, float altura)
-//        {
-//            _nome = nome;
-//            _idade = idade;
-//            _peso = peso;
-//            _altura = altura;
-//        }
-//        public void AlterarNome(string nome)
-//        {
-//            _nome = nome;
-//        }
-//        public void LerNome()
-//        {
-//            Console.WriteLine(_nome);
-//        }
-//        public void AlterarIdade(int idade)
-//        {
-//            _idade = idade;
-//        }
-//        public void LerIdade()
-//        {
-//            Console.WriteLine(_idade);
-//        }
-//        public void AlterarPeso(float peso)
-//        {
-//            _peso = _peso;
-//        }
-//        public void LerPeso()
-//        {
-//            Console.WriteLine(_peso);
-//        }
-//        public void AlterarAltura(float altura)
-//        {
-//            _altura = altura;
-//        }
-//        public void LerAltura()
-//        {
-//            Console.WriteLine(_altura);
-//        }
-//    }
-//    public class Program
-//    {
-//        static void Main()
-//        {
-//            Pessoa pessoa = new Pessoa("Guilherme", 21, 64.3f, 1.80f);
-//            pessoa.LerNome();
-//            pessoa.LerIdade();
-//            pessoa.LerPeso();
-//            pessoa.LerAltura();
-//            Console.WriteLine();
-//            pessoa.AlterarNome("João");
-//            pessoa.AlterarIdade(19);
-//            pessoa.AlterarPeso(77.3f);
-//            pessoa.AlterarAltura(1.73f);
-//            pessoa.LerNome();
-//            pessoa.LerIdade();
-//            pessoa.LerPeso();
-//            pessoa.LerAltura();
-//        }
-//    }
-//}
+namespace Program
+{
+    public class Pessoa
+    {
+        private string _nome;
+        private int _idade;
+        private float _peso;
+        private float _altura;
+        public Pessoa(string nome, int idade, float peso, float altura)
+        {
+            _nome = nome;
+            _idade = idade;
+            _peso = peso;
+            _altura = altura;
+        }
+        public void AlterarNome(string nome)
+        {
+            _nome = nome;
+        }
+        public void LerNome()
+        {
+            Console.WriteLine(_nome);
+        }
+        public void AlterarIdade(int idade)
+        {
+            _idade = idade;
+        }
+        public void LerIdade()
+        {
+            Console.WriteLine(_idade);
+        }
+        public void AlterarPeso(float peso)
+        {
+            _peso = _peso;
+        }
+        public void LerPeso()
+        {
+            Console.WriteLine(_peso);
+        }
+        public void AlterarAltura(float altura)
+        {
+            _altura = altura;
+        }
+        public void LerAltura()
+        {
+            Console.WriteLine(_altura);
+        }
+    }
+    public class Program
+    {
+        static void Main()
+        {
+            Pessoa pessoa = new Pessoa("Guilherme", 21, 64.3f, 1.80f);
+            pessoa.LerNome();
+            pessoa.LerIdade();
+            pessoa.LerPeso();
+            pessoa.LerAltura();
+            Console.WriteLine();
+            pessoa.AlterarNome("João");
+            pessoa.AlterarIdade(19);
+            pessoa.AlterarPeso(77.3f);
+            pessoa.AlterarAltura(1.73f);
+            pessoa.LerNome();
+            pessoa.LerIdade();
+            pessoa.LerPeso();
+            pessoa.LerAltura();
+        }
+    }
+}
 
-///*
-//Altere a classe “Pessoa” do exercício anterior de modo que ela seja capaz
-//de calcular o Índice de Massa Corporal (IMC). O cálculo é feito através da
-//fórmula IMC = peso/altura2. Adicionalmente, implemente um método que
-//informa a faixa de categoria do IMC que a pessoa se encontra, utilizando a
-//seguinte tabela:
+/*
+Altere a classe “Pessoa” do exercício anterior de modo que ela seja capaz
+de calcular o Índice de Massa Corporal (IMC). O cálculo é feito através da
+fórmula IMC = peso/altura2. Adicionalmente, implemente um método que
+informa a faixa de categoria do IMC que a pessoa se encontra, utilizando a
+seguinte tabela:
 
-//    Peso          Categoria
-//--------------------------------
-//   < 20         Abaixo do peso
-// > 20 e <=25     Peso normal
-// >25 e <= 30      Sobrepeso
-//> 30 e <=35    Obesidade grau I
-//> 35 e <=40    Obesidade grau II
-//   > 40        Obesidade grau III
-//--------------------------------
-//*/
+    Peso          Categoria
+--------------------------------
+   < 20         Abaixo do peso
+ > 20 e <=25     Peso normal
+ >25 e <= 30      Sobrepeso
+> 30 e <=35    Obesidade grau I
+> 35 e <=40    Obesidade grau II
+   > 40        Obesidade grau III
+--------------------------------
+*/
 
-//namespace Program
-//{
-//    public class Pessoa
-//    {
-//        private string _nome;
-//        private int _idade;
-//        private float _peso;
-//        private float _altura;
-//        private float _IMC;
-//        public Pessoa(string nome, int idade, float peso, float altura)
-//        {
-//            _nome = nome;
-//            _idade = idade;
-//            _peso = peso;
-//            _altura = altura;
-//            _IMC = _peso / (_altura * _altura);
-//        }
-//        public void AlterarNome(string nome)
-//        {
-//            _nome = nome;
-//        }
-//        public void LerNome()
-//        {
-//            Console.WriteLine(_nome);
-//        }
-//        public void AlterarIdade(int idade)
-//        {
-//            _idade = idade;
-//        }
-//        public void LerIdade()
-//        {
-//            Console.WriteLine(_idade);
-//        }
-//        public void AlterarPeso(float peso)
-//        {
-//            _peso = peso;
-//        }
-//        public void LerPeso()
-//        {
-//            Console.WriteLine(_peso);
-//        }
-//        public void AlterarAltura(float altura)
-//        {
-//            _altura = altura;
-//        }
-//        public void LerAltura()
-//        {
-//            Console.WriteLine(_altura);
-//        }
-//        public void InformarCategoriaIMC()
-//        {
-//            string categoria = "";
-//            if (_IMC > 40) categoria = "Obesidade grau III";
-//            else if (_IMC > 35) categoria = "Obesidade grau II";
-//            else if (_IMC > 30) categoria = "Obesidade grau I";
-//            else if (_IMC > 25) categoria = "Sobrepeso";
-//            else if (_IMC > 20) categoria = "Peso normal";
-//            else categoria = "Abaixo do Peso";
-//            Console.WriteLine(categoria);
-//        }
-//    }
-//    public class Program
-//    {
-//        static void Main()
-//        {
-//            Pessoa pessoa = new Pessoa("Guilherme", 21, 64.3f, 1.80f);
-//            pessoa.LerNome();
-//            pessoa.LerIdade();
-//            pessoa.LerPeso();
-//            pessoa.LerAltura();
-//            pessoa.InformarCategoriaIMC();
-//            Console.WriteLine();
-//            pessoa.AlterarNome("João");
-//            pessoa.AlterarIdade(19);
-//            pessoa.AlterarPeso(87.3f);
-//            pessoa.AlterarAltura(1.73f);
-//            pessoa.LerNome();
-//            pessoa.LerIdade();
-//            pessoa.LerPeso();
-//            pessoa.LerAltura();
-//            pessoa.InformarCategoriaIMC();
-//        }
-//    }
-//}
+namespace Program
+{
+    public class Pessoa
+    {
+        private string _nome;
+        private int _idade;
+        private float _peso;
+        private float _altura;
+        private float _IMC;
+        public Pessoa(string nome, int idade, float peso, float altura)
+        {
+            _nome = nome;
+            _idade = idade;
+            _peso = peso;
+            _altura = altura;
+            _IMC = _peso / (_altura * _altura);
+        }
+        public void AlterarNome(string nome)
+        {
+            _nome = nome;
+        }
+        public void LerNome()
+        {
+            Console.WriteLine(_nome);
+        }
+        public void AlterarIdade(int idade)
+        {
+            _idade = idade;
+        }
+        public void LerIdade()
+        {
+            Console.WriteLine(_idade);
+        }
+        public void AlterarPeso(float peso)
+        {
+            _peso = peso;
+        }
+        public void LerPeso()
+        {
+            Console.WriteLine(_peso);
+        }
+        public void AlterarAltura(float altura)
+        {
+            _altura = altura;
+        }
+        public void LerAltura()
+        {
+            Console.WriteLine(_altura);
+        }
+        public void InformarCategoriaIMC()
+        {
+            string categoria = "";
+            if (_IMC > 40) categoria = "Obesidade grau III";
+            else if (_IMC > 35) categoria = "Obesidade grau II";
+            else if (_IMC > 30) categoria = "Obesidade grau I";
+            else if (_IMC > 25) categoria = "Sobrepeso";
+            else if (_IMC > 20) categoria = "Peso normal";
+            else categoria = "Abaixo do Peso";
+            Console.WriteLine(categoria);
+        }
+    }
+    public class Program
+    {
+        static void Main()
+        {
+            Pessoa pessoa = new Pessoa("Guilherme", 21, 64.3f, 1.80f);
+            pessoa.LerNome();
+            pessoa.LerIdade();
+            pessoa.LerPeso();
+            pessoa.LerAltura();
+            pessoa.InformarCategoriaIMC();
+            Console.WriteLine();
+            pessoa.AlterarNome("João");
+            pessoa.AlterarIdade(19);
+            pessoa.AlterarPeso(87.3f);
+            pessoa.AlterarAltura(1.73f);
+            pessoa.LerNome();
+            pessoa.LerIdade();
+            pessoa.LerPeso();
+            pessoa.LerAltura();
+            pessoa.InformarCategoriaIMC();
+        }
+    }
+}
 
-///*
-//Escreva um programa que leia o valor de um capital investido e a taxa 
-//de juros mensais. Supondo que essa taxa seja constante ao longo de um ano, 
-//apresente o valor do investimento ao final de cada mês durante um ano.
-//*/
+/*
+Escreva um programa que leia o valor de um capital investido e a taxa 
+de juros mensais. Supondo que essa taxa seja constante ao longo de um ano, 
+apresente o valor do investimento ao final de cada mês durante um ano.
+*/
 
-//namespace Program
-//{
-//    class Investimento
-//    {
-//        private double _valor;
-//        private float _taxaDeJuros;
-//        public Investimento(double valor, float taxadeJuros)
-//        {
-//            _valor = valor;
-//            _taxaDeJuros = (taxadeJuros / 100f);
-//        }
-//        public void AcompanharInvestimento()
-//        {
-//            for (int i = 0; i < 12; i++)
-//            {
-//                _valor += _valor * _taxaDeJuros;
-//                Console.WriteLine("Valor no {0}º mês: {1}", i + 1,
-//                _valor);
-//            }
-//        }
-//    }
-//    class Program
-//    {
-//        static void Main()
-//        {
-//            Investimento investimento = new Investimento(1000.00,
-//            15.3f);
-//            investimento.AcompanharInvestimento();
-//        }
-//    }
-//}
+namespace Program
+{
+    class Investimento
+    {
+        private double _valor;
+        private float _taxaDeJuros;
+        public Investimento(double valor, float taxadeJuros)
+        {
+            _valor = valor;
+            _taxaDeJuros = (taxadeJuros / 100f);
+        }
+        public void AcompanharInvestimento()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                _valor += _valor * _taxaDeJuros;
+                Console.WriteLine("Valor no {0}º mês: {1}", i + 1,
+                _valor);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Investimento investimento = new Investimento(1000.00,
+            15.3f);
+            investimento.AcompanharInvestimento();
+        }
+    }
+}
 
 /* Resolver o problema 2787 da Plataforma BeeCrowd. */
 
